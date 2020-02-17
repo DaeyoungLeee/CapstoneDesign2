fc05413d69b40d1a9577380971a78e6c5ae91d34
daeyoung <eo930827@naver.com>
2020-02-17 22:26:50 +0900

    최대힘 측정

---
+++ app/src/main/java/kr/ac/yonsei/capstonedesign2/DrawerLayout_Contents/Set1_Activity.java
@@ -0,0 +1,14 @@
+package kr.ac.yonsei.capstonedesign2.DrawerLayout_Contents;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Set1_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
